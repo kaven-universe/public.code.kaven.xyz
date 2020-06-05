62d6e2934d3e3ae5bc8336161576c8006965bdd7
Kaven <kaven@wuwenkai.com>
2020-06-05 17:54:42 +0800

    list files of  a ftp directory

---
+++ get_file_from_ftp.py
@@ -0,0 +1,31 @@
+from ftplib import FTP
+import re
+
+ftp = FTP('192.168.1.201')
+ftp.login()
+
+ftp.cwd('Packages/rar_EDM_2/')
+
+files = []
+
+
+def parse(p):
+    file_name = p.split(' ')[-1]
+    if file_name != '.' and file_name != '..':
+        files.append(file_name)
+
+
+ftp.retrlines('LIST', parse)
+
+for name in files:
+    try:
+        ftp.cwd(name)
+        # print('dir: '+name)
+    except:
+        sr = re.search(r'.*((\d+\.)(\d+\.)(\d+\.)(\d)).*', name)
+        if sr is not None:
+            version = sr.group(1)
+            print('file: '+name+', version: '+version)
+    else:
+        # 打开路径没问题，类型是文件夹，返回上一级
+        ftp.cwd('..')

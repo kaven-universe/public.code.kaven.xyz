a952103900598d76d5c6457dd23279ac8aca6ddc
Kaven <kaven@wuwenkai.com>
2020-06-05 17:58:21 +0800

    get latest version(temp)

--- get_file_from_ftp.py
+++ get_file_from_ftp.py
@@ -17,6 +17,8 @@ def parse(p):
 
 ftp.retrlines('LIST', parse)
 
+latest = '0.0.0.0'
+
 for name in files:
     try:
         ftp.cwd(name)
@@ -26,6 +28,11 @@ for name in files:
         if sr is not None:
             version = sr.group(1)
             print('file: '+name+', version: '+version)
+
+            if version > latest:
+                latest=name
     else:
         # 打开路径没问题，类型是文件夹，返回上一级
         ftp.cwd('..')
+
+print('latest: '+latest)

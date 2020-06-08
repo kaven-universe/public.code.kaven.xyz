037ad02265b66c258fb200d38b01d70225ce583d
Kaven <kaven@wuwenkai.com>
2020-06-08 11:11:51 +0800

    get latest version

--- get_file_from_ftp.py
+++ get_file_from_ftp.py
@@ -8,6 +8,14 @@ ftp.cwd('Packages/rar_EDM_2/')
 
 files = []
 
+def compare_version(a, b):
+    sp_a = a.split('.')
+    sp_b = b.split('.')
+    for i in range(0, min(len(sp_a), len(sp_b))):
+        if sp_a[i] != sp_b[i]:
+            return int(sp_a[i]) - int(sp_b[i])
+    return 0    
+    
 
 def parse(p):
     file_name = p.split(' ')[-1]
@@ -29,8 +37,8 @@ for name in files:
             version = sr.group(1)
             print('file: '+name+', version: '+version)
 
-            if version > latest:
-                latest=name
+            if compare_version(version, latest) > 0:
+                latest=version
     else:
         # 打开路径没问题，类型是文件夹，返回上一级
         ftp.cwd('..')

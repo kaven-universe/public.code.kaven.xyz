1cdd43ba5e2d8b1a2b5ff841cbae8059824a7cf5
Kaven <kaven@wuwenkai.com>
2021-02-10 13:28:41 +0800

    Update Gogs.js

--- JavaScript/Gogs.js
+++ JavaScript/Gogs.js
@@ -4,10 +4,10 @@
  * @website:     http://blog.kaven.xyz
  * @file:        [Kaven-Common] /JavaScript/Gogs.js
  * @create:      2021-02-05 20:28:58.404
- * @modify:      2021-02-05 22:40:20.127
+ * @modify:      2021-02-10 13:28:32.701
  * @version:     
- * @times:       5
- * @lines:       30
+ * @times:       6
+ * @lines:       38
  * @copyright:   Copyright © 2021 Kaven. All Rights Reserved.
  * @description: [description]
  * @license:     [license]
@@ -24,7 +24,14 @@ function GogsTransferOwnership(new_owner_name) {
 
 GogsTransferOwnership(`Forked`);
 
-const base = `https://kaven.xyz:3000`;
-const first_href = $(`.ui.repository.list a`).first().attr('href');
-const url = base + first_href;
-return url;
+function First() {
+    const base = `https://kaven.xyz:3000`;
+    const first_href = $(`.ui.repository.list a`).first().attr('href');
+    const url = base + first_href;
+    return url;
+}
+
+
+copy($("#repo-files-table .name a").map(function(){ 
+    return $(this).attr("href").replace("/Kaven/Kaven-Notes/src/master/Blog", ""); 
+}).get().join("\n"));

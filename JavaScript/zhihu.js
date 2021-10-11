279598424c8cc130c08b06bb299352349317b475
Kaven <kaven@wuwenkai.com>
2021-10-11 13:50:56 +0800

    Update zhihu.js

--- JavaScript/zhihu.js
+++ JavaScript/zhihu.js
@@ -4,10 +4,10 @@
  * @website:     http://blog.kaven.xyz
  * @file:        [Kaven-Common] /JavaScript/zhihu.js
  * @create:      2021-10-11 11:20:31.863
- * @modify:      2021-10-11 13:45:55.767
+ * @modify:      2021-10-11 13:50:43.292
  * @version:     
- * @times:       8
- * @lines:       62
+ * @times:       9
+ * @lines:       44
  * @copyright:   Copyright © 2021 Kaven. All Rights Reserved.
  * @description: [description]
  * @license:     [license]
@@ -19,37 +19,19 @@ function main() {
     // hide login
     document.querySelector("body > div:nth-child(14) > div > div > div > div.Modal.Modal--default.signFlowModal > button").click();
 
-    document.addEventListener("click", function (e) {
-        console.log(e);
+    const windowOpen = window.open;
+    const prefix = "https://link.zhihu.com/?target=";
 
-        /**
-         * @type { string | undefined }
-         */
-        const tagName = e.target.tagName;
-        if (tagName?.toLowerCase() === "a") {
+    window.open = function (url, name, features, replace) {
 
-            /**
-             * @type { string }
-             */
-            const href = e.target.href;
-
-            if (href) {
-                const prefix = "https://link.zhihu.com/?target=";
-
-                let url = href;
-                if (url.startsWith(prefix)) {
-                    url = url.substr(prefix.length);
-                }
-
-                console.log("open: ", url);
-
-                window.open(url, "_blank").focus();
-                e.preventDefault();
-            } else {
-                console.warn(e);
-            }
+        if (url.startsWith(prefix)) {
+            url = url.substr(prefix.length);
         }
-    });
+
+        console.log("open: ", url);
+
+        windowOpen(url, name, features, replace);
+    }
 }
 
 const i = setInterval(() => {

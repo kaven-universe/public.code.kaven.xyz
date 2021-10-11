4aca2315fe21809a712ce5b5b267a3a583b35f14
Kaven <kaven@wuwenkai.com>
2021-10-11 13:12:42 +0800

    Update zhihu.js

--- JavaScript/zhihu.js
+++ JavaScript/zhihu.js
@@ -4,10 +4,10 @@
  * @website:     http://blog.kaven.xyz
  * @file:        [Kaven-Common] /JavaScript/zhihu.js
  * @create:      2021-10-11 11:20:31.863
- * @modify:      2021-10-11 11:29:47.236
+ * @modify:      2021-10-11 13:11:36.797
  * @version:     
- * @times:       4
- * @lines:       28
+ * @times:       5
+ * @lines:       35
  * @copyright:   Copyright © 2021 Kaven. All Rights Reserved.
  * @description: [description]
  * @license:     [license]
@@ -15,7 +15,14 @@
 
 function main() {
     console.log("kaven script loaded");
+
+    // hide login
     document.querySelector("body > div:nth-child(14) > div > div > div > div.Modal.Modal--default.signFlowModal > button").click();
+
+    document.addEventListener("click", function(e) {
+        console.log(this, e);
+        e.preventDefault();
+    });
 }
 
 const i = setInterval(() => {

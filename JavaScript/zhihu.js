834fc5c93e9faf08d706765a28821a361281a5e6
Kaven <kaven@wuwenkai.com>
2021-10-11 11:20:59 +0800

    Create zhihu.js

---
+++ JavaScript/zhihu.js
@@ -0,0 +1,18 @@
+/********************************************************************
+ * @author:      Kaven
+ * @email:       kaven@wuwenkai.com
+ * @website:     http://blog.kaven.xyz
+ * @file:        [Kaven-Common] /JavaScript/zhihu.js
+ * @create:      2021-10-11 11:20:31.863
+ * @modify:      2021-10-11 11:20:31.863
+ * @version:     
+ * @times:       1
+ * @lines:       4
+ * @copyright:   Copyright © 2021 Kaven. All Rights Reserved.
+ * @description: [description]
+ * @license:     [license]
+ ********************************************************************/
+
+$(() => {
+    $("body > div:nth-child(14) > div > div > div > div.Modal.Modal--default.signFlowModal > button").click();
+});

55aafb8f33ff666b67de91e292658dc395e6b9dd
Kaven <kaven@wuwenkai.com>
2022-04-22 14:44:14 +0800

    Wrap to a class

--- JavaScript/Kaven.js
+++ JavaScript/Kaven.js
@@ -4,10 +4,10 @@
  * @website:     http://blog.kaven.xyz
  * @file:        [Kaven-Common] /JavaScript/Kaven.js
  * @create:      2021-10-11 11:20:31.863
- * @modify:      2022-01-10 17:51:51.764
+ * @modify:      2022-04-22 14:43:40.964
  * @version:     
- * @times:       38
- * @lines:       207
+ * @times:       40
+ * @lines:       232
  * @copyright:   Copyright © 2021-2022 Kaven. All Rights Reserved.
  * @description: [description]
  * @license:     [license]
@@ -15,193 +15,218 @@
 
 const K_DEV = "K_DEV";
 
-function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
+class Kaven {
 
-function getCookie(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(";");
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == " ") c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-}
-
-function DEV(enable) {
-    if (enable === undefined) {
-        return getCookie(K_DEV) === "true";
+    static setCookie(name, value, days) {
+        let expires = "";
+        if (days) {
+            const date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
     }
 
-    if (enable) {
-        setCookie(K_DEV, "true", 1);
-    } else {
-        eraseCookie(K_DEV);
-    }
-}
-
-
-/**
- * 
- * @param { String } h 
- * @returns 
- */
-function checkHostName(h) {
-    const hostname = window.location.hostname.toLowerCase();
-
-    if (hostname === h) {
-        return true;
+    static getCookie(name) {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(";");
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == " ") c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
     }
 
-    if (hostname.endsWith(`.${h}`)) {
-        return true;
+    static eraseCookie(name) {
+        document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
     }
 
-    return false;
-}
-
-function click(selector) {
-    const el = document.querySelector(selector);
-    if (el) {
-        el.click();
-        return true;
-    }
-
-    return false;
-}
-
-function main() {
-    const isZhihu = checkHostName("zhihu.com");
-    const isCSDN = checkHostName("csdn.net");
-    const isJianshu = checkHostName("jianshu.com");
-
-    /**
-     * @type { Set<string> }
-     */
-    const prefixSet = new Set();
-
-    if (isZhihu) {
-        prefixSet.add("https://link.zhihu.com/?target=");
-        prefixSet.add("https://link.zhihu.com?target=");
-
-        // hide login
-        let i = 10;
-        do {
-            if (click(`body > div:nth-child(${i}) > div > div > div > div.Modal.Modal--default.signFlowModal > button`)) {
-                break;
-            }
-
-            i++;
-        } while (i < 30);
-
-    } else if (isCSDN) {
-        prefixSet.add("https://link.csdn.net/?target=");
-        prefixSet.add("https://link.csdn.net?target=");
-
-        // hide top bar
-        click("#csdn-toolbar > div.toolbar-advert > span");
-
-        click("#csdn-redpack > em");
-        click("#csdn-redpack > div > em");
-    } else if (isJianshu) {
-        prefixSet.add("https://link.jianshu.com/?t=");
-        prefixSet.add("https://links.jianshu.com/go?to=");
-    } else {
-        return;
-    }
-
-    const windowOpen = window.open;
-    // const iframe = document.createElement("iframe");
-    // document.body.appendChild(iframe);
-    // const windowOpen = iframe.contentWindow.open;
-
-    window.open = function(url, name, features, replace) {
-
-        for (const prefix of prefixSet) {
-            if (url.startsWith(prefix)) {
-                url = url.substr(prefix.length);
-            }
+    static DEV(enable) {
+        if (enable === undefined) {
+            return Kaven.getCookie(K_DEV) === "true";
         }
 
-        url = decodeURIComponent(url);
-
-        if (DEV()) {
-            console.log("open: ", url);
+        if (enable) {
+            Kaven.setCookie(K_DEV, "true", 1);
+        } else {
+            Kaven.eraseCookie(K_DEV);
         }
+    }
 
-        return windowOpen(url, name, features, replace);
-    };
 
     /**
      * 
-     * @param { HTMLLinkElement } target 
+     * @param { String } h 
+     * @returns 
      */
-    const checkLink = (target) => {
+    static checkHostName(h) {
+        const hostname = window.location.hostname.toLowerCase();
 
-        if (!target) {
-            return false;
+        if (hostname === h) {
+            return true;
         }
 
-        if (DEV()) {
-            console.log("check: ", target);
-        }
-
-        const tagName = target.tagName;
-        if (tagName?.toLowerCase() === "a") {
-
-            const href = target.href;
-
-            if (href) {
-                window.open(href, "_blank")?.focus();
-                return true;
-            }
+        if (hostname.endsWith(`.${h}`)) {
+            return true;
         }
 
         return false;
-    };
+    }
 
-    document.addEventListener("click", function(e) {
-        if (DEV()) {
-            console.log(e);
+    static click(selector) {
+        const el = document.querySelector(selector);
+        if (el) {
+            el.click();
+            return true;
         }
 
-        if (checkLink(e.target)) {
-            e.preventDefault();
-        } else {
-            const path = e.path;
-            if (Array.isArray(path) && path.length > 1) {
-                let i = 1;
-                do {
-                    if (checkLink(path[i])) {
-                        e.preventDefault();
-                        break;
-                    }
-                } while (i++ < 5);
+        return false;
+    }
+
+    static getColumnText(tableSelector, col) {
+        const tab = document.querySelector(tableSelector);
+        const n = tab.rows.length;
+
+        const textList = [];
+
+        // First check that col is not less then 0
+        if (col < 0) {
+            return textList;
+        }
+
+        for (let i = 0; i < n; i++) {
+            const tr = tab.rows[i];
+            if (tr.cells.length > col) {
+                const td = tr.cells[col];
+                textList.push(td.innerText);
             }
         }
-    });
+
+        return textList;
+    }
+
+    static main() {
+        const isZhihu = Kaven.checkHostName("zhihu.com");
+        const isCSDN = Kaven.checkHostName("csdn.net");
+        const isJianshu = Kaven.checkHostName("jianshu.com");
+
+        /**
+         * @type { Set<string> }
+         */
+        const prefixSet = new Set();
+
+        if (isZhihu) {
+            prefixSet.add("https://link.zhihu.com/?target=");
+            prefixSet.add("https://link.zhihu.com?target=");
+
+            // hide login
+            let i = 10;
+            do {
+                if (Kaven.click(`body > div:nth-child(${i}) > div > div > div > div.Modal.Modal--default.signFlowModal > button`)) {
+                    break;
+                }
+
+                i++;
+            } while (i < 30);
+
+        } else if (isCSDN) {
+            prefixSet.add("https://link.csdn.net/?target=");
+            prefixSet.add("https://link.csdn.net?target=");
+
+            // hide top bar
+            Kaven.click("#csdn-toolbar > div.toolbar-advert > span");
+
+            Kaven.click("#csdn-redpack > em");
+            Kaven.click("#csdn-redpack > div > em");
+        } else if (isJianshu) {
+            prefixSet.add("https://link.jianshu.com/?t=");
+            prefixSet.add("https://links.jianshu.com/go?to=");
+        } else {
+            return;
+        }
+
+        const windowOpen = window.open;
+        // const iframe = document.createElement("iframe");
+        // document.body.appendChild(iframe);
+        // const windowOpen = iframe.contentWindow.open;
+
+        window.open = function(url, name, features, replace) {
+
+            for (const prefix of prefixSet) {
+                if (url.startsWith(prefix)) {
+                    url = url.substr(prefix.length);
+                }
+            }
+
+            url = decodeURIComponent(url);
+
+            if (Kaven.DEV()) {
+                console.log("open: ", url);
+            }
+
+            return windowOpen(url, name, features, replace);
+        };
+
+        /**
+         * 
+         * @param { HTMLLinkElement } target 
+         */
+        const checkLink = (target) => {
+
+            if (!target) {
+                return false;
+            }
+
+            if (Kaven.DEV()) {
+                console.log("check: ", target);
+            }
+
+            const tagName = target.tagName;
+            if (tagName?.toLowerCase() === "a") {
+
+                const href = target.href;
+
+                if (href) {
+                    window.open(href, "_blank")?.focus();
+                    return true;
+                }
+            }
+
+            return false;
+        };
+
+        document.addEventListener("click", function(e) {
+            if (Kaven.DEV()) {
+                console.log(e);
+            }
+
+            if (checkLink(e.target)) {
+                e.preventDefault();
+            } else {
+                const path = e.path;
+                if (Array.isArray(path) && path.length > 1) {
+                    let i = 1;
+                    do {
+                        if (checkLink(path[i])) {
+                            e.preventDefault();
+                            break;
+                        }
+                    } while (i++ < 5);
+                }
+            }
+        });
+    }
 }
 
 if (document.readyState === "complete") {
-    main();
+    Kaven.main();
 } else {
     const i = setInterval(() => {
         if (document.readyState === "complete") {
             clearInterval(i);
 
-            main();
+            Kaven.main();
         }
     }, 850);
 }

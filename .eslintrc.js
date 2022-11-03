5c62f92602d3d5d415ef8c3433c9ba276a0c16b0
Kaven <kaven@wuwenkai.com>
2022-11-03 18:00:08 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,9 @@
  * @website:     http://blog.kaven.xyz
  * @file:        [kaven-public] /.eslintrc.js
  * @create:      2021-07-07 07:42:19.333
- * @modify:      2022-07-11 18:27:36.200
- * @times:       4
- * @lines:       31
+ * @modify:      2022-11-03 17:59:37.712
+ * @times:       5
+ * @lines:       32
  * @copyright:   Copyright © 2021-2022 Kaven. All Rights Reserved.
  * @description: [description]
  * @license:     [license]
@@ -16,6 +16,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        JQuery: true,
     },
     // "extends": "eslint:recommended",
     extends: [

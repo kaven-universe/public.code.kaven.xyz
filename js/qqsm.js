389a1834d7010d1e5acde7f8d4bf3012d936d2f9
Kaven <kaven@wuwenkai.com>
2023-01-22 16:47:13 +0800

    Update qqsm.js

--- js/qqsm.js
+++ js/qqsm.js
@@ -4,9 +4,9 @@
  * @website:     http://blog.kaven.xyz
  * @file:        [kaven-public] /js/qqsm.js
  * @create:      2023-01-22 16:44:38.451
- * @modify:      2023-01-22 16:44:38.451
- * @times:       1
- * @lines:       18
+ * @modify:      2023-01-22 16:47:10.056
+ * @times:       2
+ * @lines:       42
  * @copyright:   Copyright © 2023 Kaven. All Rights Reserved.
  * @description: [description]
  * @license:     [license]
@@ -15,6 +15,16 @@
 class QQSM {
     static id;
 
+    static click(selector) {
+        const el = document.querySelector(selector);
+        if (el) {
+            el.click();
+            return true;
+        }
+
+        return false;
+    }
+
     static start(timeout = 1000) {
         if (this.id) {
             return;
@@ -22,7 +32,7 @@ class QQSM {
 
         this.isRunning = setInterval(() => {                
             layer.closeAll();
-            Kaven.click("#hb > dl:nth-child(1) > dt");                
+            this.click("#hb > dl:nth-child(1) > dt");                
         }, timeout);            
     }
 

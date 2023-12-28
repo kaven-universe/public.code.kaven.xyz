fcdf7fb0c7e2377d4b32b8cf038d884662269c43
Kaven <kaven@wuwenkai.com>
2023-12-28 17:51:41 +0800

    Update proxy.mjs

--- mjs/proxy.mjs
+++ mjs/proxy.mjs
@@ -4,9 +4,9 @@
  * @website:     http://blog.kaven.xyz
  * @file:        [kaven-public] /mjs/proxy.mjs
  * @create:      2023-12-28 15:37:23.256
- * @modify:      2023-12-28 16:21:06.789
- * @times:       5
- * @lines:       68
+ * @modify:      2023-12-28 17:32:21.177
+ * @times:       8
+ * @lines:       80
  * @copyright:   Copyright © 2023 Kaven. All Rights Reserved.
  * @description: [description]
  * @license:     [license]
@@ -18,50 +18,62 @@ import { createConnection } from "node:net";
 
 const proxies = [];
 
-const url = "https://gitee.com/mirrors_TheSpeedX/PROXY-List/raw/master/socks5.txt"; // "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt"
-const data = await HttpTryGetText(url);
-const lines = SplitStringByNewline(data);
-if (lines && lines.length > 0) {
-    for (const line of lines) {
-        const [host, port] = line.split(":");
+async function add(host, port, type, tags) {
+    const socket = createConnection(port, host);
 
-        const socket = createConnection(port, host);
+    // Event: Connection established
+    socket.on('connect', () => {
+        console.log(`TCP server at ${host}:${port} is available.`);
+        socket.end(); // Close the connection after testing
 
-        // Event: Connection established
-        socket.on('connect', () => {
-            console.log(`TCP server at ${host}:${port} is available.`);
-            socket.end(); // Close the connection after testing
+        if (typeof tags === "string") {
+            tags = [tags];
+        } else if (!Array.isArray(tags)) {
+            tags = [];
+        }
 
-            proxies.push({
-                "Type": "Socks5",
-                "Host": host,
-                "Port": Number(port),
-                "Tags": [
-                    "Socks5",
-                    "TheSpeedX"
-                ],
-                "UserName": null,
-                "Password": null,
-                "IgnoreServerCertificateVerification": false
-            });
+        proxies.push({
+            "Type": type,
+            "Host": host,
+            "Port": Number(port),
+            "Tags": tags,
+            "UserName": null,
+            "Password": null,
+            "IgnoreServerCertificateVerification": false
         });
+    });
 
-        // Event: Connection error
-        socket.on('error', (err) => {
-            console.error(`Error connecting to TCP server: ${err.message}`);
-        });
+    // Event: Connection error
+    socket.on('error', (err) => {
+        console.error(`Error connecting to TCP server: ${err.message}`);
+    });
 
-        // Event: Connection timeout
-        socket.on('timeout', () => {
-            console.error(`Connection to TCP server timed out.`);
-            socket.destroy(); // Destroy the socket explicitly to ensure it is closed
-        });
+    // Event: Connection timeout
+    socket.on('timeout', () => {
+        console.error(`Connection to TCP server timed out.`);
+        socket.destroy(); // Destroy the socket explicitly to ensure it is closed
+    });
 
-        // Set a timeout for the connection attempt
-        socket.setTimeout(1000);
-    }
-
-    await Sleep(2000);
+    // Set a timeout for the connection attempt
+    socket.setTimeout(1000);
 }
 
-await SaveJsonConfig(proxies, "./generated/fixed-proxy.json");
+async function addSocks5ProxyFromUrl(url, tags) {
+    const data = await HttpTryGetText(url);
+    const lines = SplitStringByNewline(data);
+    if (lines && lines.length > 0) {
+        for (const line of lines) {
+            const [host, port] = line.split(":");
+            add(host, port, "Socks5", tags);
+        }
+    }
+}
+
+// await addSocks5ProxyFromUrl("https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt", ["free", "public"]);
+//await addSocks5ProxyFromUrl("https://gitee.com/mirrors_TheSpeedX/PROXY-List/raw/master/socks5.txt", ["free", "public"]);
+
+// await addSocks5ProxyFromUrl("https://github.com/monosans/proxy-list/blob/main/proxies_anonymous/socks5.txt", ["free", "public"]);
+await addSocks5ProxyFromUrl("https://gitee.com/git-sync/proxy-list/raw/main/proxies_anonymous/socks5.txt", ["free", "public"]);
+
+await Sleep(2000);
+await SaveJsonConfig(proxies, "./generated/proxies.json");

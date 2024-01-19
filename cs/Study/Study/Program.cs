18b670a8dd99b37883f7c5ab90b172538558b3c9
Kaven <kaven@wuwenkai.com>
2024-01-19 12:23:31 +0800

    Add Study Project

---
+++ cs/Study/Study/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Study
+{
+    public class A
+    {
+        public virtual int Id { get; set; }
+    }
+
+    public class B : A
+    {
+        public override int Id
+        {
+            get;
+            set;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var b = new B()
+            {
+                Id = 1,
+            };
+
+            Console.WriteLine(b.Id);
+
+            A a = b;
+
+            Console.WriteLine(a.Id);
+            Console.ReadLine();
+        }
+    }
+}

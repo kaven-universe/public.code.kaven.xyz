16196b6112fbf6a1b124f4a0bd05ff570f9e5569
Kaven <kaven@wuwenkai.com>
2024-01-24 14:01:40 +0800

    Update Program.cs

--- cs/Study/Study/Program.cs
+++ cs/Study/Study/Program.cs
@@ -1,33 +1,11 @@
 ﻿namespace Study
 {
-    public class A
-    {
-        public virtual int Id { get; set; }
-    }
-
-    public class B : A
-    {
-        public override int Id
-        {
-            get;
-            set;
-        }
-    }
-
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            var b = new B()
-            {
-                Id = 1,
-            };
+            Inheritance.Run();
 
-            Console.WriteLine(b.Id);
-
-            A a = b;
-
-            Console.WriteLine(a.Id);
             Console.ReadLine();
         }
     }

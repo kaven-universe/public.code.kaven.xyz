87169b18ae00ba4de64c6ae97e2299b2b844d63f
Kaven <kaven@wuwenkai.com>
2024-01-24 14:01:38 +0800

    Create Inheritance.cs

---
+++ cs/Study/Study/Inheritance.cs
@@ -0,0 +1,33 @@
+﻿namespace Study
+{
+    public class Inheritance
+    {
+        public class A
+        {
+            public virtual int Id { get; set; }
+        }
+
+        public class B : A
+        {
+            public override int Id
+            {
+                get;
+                set;
+            }
+        }
+
+        public static void Run()
+        {
+            var b = new B()
+            {
+                Id = 1,
+            };
+
+            Console.WriteLine(b.Id);
+
+            A a = b;
+
+            Console.WriteLine(a.Id);
+        }
+    }
+}

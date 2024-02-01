f48bdd8e9e7dba57889f16f9f4138e8069c96636
Kaven <kaven@wuwenkai.com>
2024-02-01 18:18:12 +0800

    Create FloatingPoint.cs

---
+++ cs/Study/Study/FloatingPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace Study
+{
+    public class FloatingPoint
+    {
+        public static void Run()
+        {
+            var v = Math.Pow(2, 24); ;
+
+            var f = Convert.ToSingle(v);
+
+            Debug.Assert(f == v);
+
+            Debug.Assert(f - 1 == v - 1);
+            Debug.Assert(f + 1 != v + 1);
+
+            var i = 10;
+            while (i-- > 0)
+            {
+                Debug.WriteLine(f++);
+            }
+
+            i = 10;
+            while (i-- > 0)
+            {
+                Debug.WriteLine(f--);
+            }
+        }
+    }
+}

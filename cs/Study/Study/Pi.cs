a36e3e6495ea2f97bff7d9eb4f2f17857ddb27ba
Kaven <kaven@wuwenkai.com>
2024-03-08 16:35:19 +0800

    Create Pi.cs

---
+++ cs/Study/Study/Pi.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Study
+{
+    public class Pi
+    {
+        public static void Run()
+        {
+            var pi = MonteCarloApproximate(1000000000);
+            Console.WriteLine(pi);
+        }
+
+        public static double MonteCarloApproximate(int numPoints)
+        {
+            var random = new Random();
+            var pointsInsideCircle = 0;
+
+            for (var i = 0; i < numPoints; i++)
+            {
+                var x = random.NextDouble() * 2 - 1; // Random x-coordinate between -1 and 1
+                var y = random.NextDouble() * 2 - 1; // Random y-coordinate between -1 and 1
+                var distanceSquared = x * x + y * y;
+
+                if (distanceSquared <= 1)
+                {
+                    pointsInsideCircle++;
+                }
+            }
+
+            return 4.0 * pointsInsideCircle / numPoints;
+        }
+    }
+}

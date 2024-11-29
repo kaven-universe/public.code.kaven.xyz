668dd590a31d96423d01474814ada03d44bcdba1
Kaven <kaven@wuwenkai.com>
2024-11-29 14:14:30 +0800

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,9 @@
  * @website:     http://blog.kaven.xyz
  * @file:        [kaven-public] /eslint.config.mjs
  * @create:      2024-11-29 11:07:57.272
- * @modify:      2024-11-29 11:12:05.546
- * @times:       5
- * @lines:       26
+ * @modify:      2024-11-29 14:14:12.036
+ * @times:       9
+ * @lines:       32
  * @copyright:   Copyright © 2024 Kaven. All Rights Reserved.
  * @description: [description]
  * @license:     [license]
@@ -17,7 +17,13 @@ import configs, { globals } from "@wenkai.wu/eslint-config";
 export default [
     ...configs,
     {
-        languageOptions: { globals: { ...globals.node } },
+        languageOptions: {
+            globals: { 
+                ...globals.node, 
+                ...globals.browser,
+                ...globals.jquery,                
+            }, 
+        },
         rules: {
             "no-console": "off",
         },
